2cba043bf4ad6ab59cdde6c73ca6f77fc7258267
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2022-01-20 02:00:36 +0200

    Update: move remark-telegraph to separate package

--- src/telegram/Telegraph.js
+++ src/telegram/Telegraph.js
@@ -1,6 +1,7 @@
 import remarkParse from 'remark-parse';
 import unified from 'unified';
-import { isFunction, isString, isObject, fill } from 'myrmidon';
+import { fill } from 'myrmidon';
+import remarkTelegraph from 'remark-telegraph';
 import Api from './TelegraphAPI';
 
 function dumpAccount(account) {
@@ -14,77 +15,6 @@ function dumpPage(page) {
     };
 }
 
-const Tags = {
-    text : node => node.value,
-
-    softbreak        : 'br',
-    linebreak        : 'br',
-    'thematic_break' : 'br',
-
-    emph   : 'em',
-    strong : 'strong',
-
-    link  : node => ({ tag: 'a', attrs: { href: node.url } }),
-    image : node => ({ tag: 'img', attrs: { href: node.url } }),
-
-    code         : node => ({ tag: 'code', children: node.value }),
-    'code_block' : node => ({ tag: 'code', children: node.value }),
-
-    'block_quote' : 'blockquote',
-
-    heading : node => ({ tag: node.depth === 1 ? 'h3' : 'h4' }),
-    list    : node => ({ tag: node.ordered ? 'ol' : 'ul' }),
-
-    listItem  : 'li',
-    paragraph : 'p'
-
-};
-
-class Visitor {
-    constructor(tags) {
-        this.tags = tags;
-    }
-
-    visit = (node) => {
-        const result = {};
-        const handler = this.tags[node.type];
-
-        if (handler) {
-            if (isFunction(handler)) {
-                const parsed = handler(node);
-
-                if (isObject(parsed)) {
-                    Object.assign(result, parsed);
-                }
-
-                if (isString(parsed)) return parsed;
-            }
-
-            if (isString(handler)) {
-                result.tag = handler;
-            }
-        }
-
-        if (node.children) {
-            result.children = node.children.map(n => this.visit(n));
-            if (node.type === 'root') return result.children;
-        }
-
-        return result;
-    }
-}
-
-
-function remarkPlugin() {
-    const visitor = new Visitor(Tags);
-
-    function Compiler(tree) {
-        return JSON.stringify(visitor.visit(tree));
-    }
-
-    Object.assign(this, { Compiler });
-}
-
 export default class Telegraph {
     constructor(token) {
         this.api = new Api(token);
@@ -96,7 +26,7 @@ export default class Telegraph {
 
         const content = await unified()
             .use(remarkParse)
-            .use(remarkPlugin)
+            .use(remarkTelegraph)
             .process(mdMessage);
 
         const page = await this.api.createPage(mdTitle, String(content));
